aaec4b1afc8d92db71b658eea89628834ff4395f
nsf <no.smile.face@gmail.com>
2008-05-26 15:22:58 -0700

    Version changed to 0.9.19.

--- src/bmpanel.c
+++ src/bmpanel.c
@@ -125,7 +125,7 @@ static int commence_panel_redraw;
 static int commence_switcher_redraw;
 
 static const char *theme = "native";
-static const char *version = "bmpanel version 0.9.18";
+static const char *version = "bmpanel version 0.9.19";
 static const char *usage = "usage: bmpanel [--version] [--help] [--usage] [--list] THEME";
 
 static void cleanup();
